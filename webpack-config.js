877b0587ef68629a2e23ba70df8a7a85b91dc88d
zelawest <169395273+ZELAWEST@users.noreply.github.com>
2024-12-26 10:53:45 +0200

    Fix webpack-config.js

--- webpack-config.js
+++ webpack-config.js
@@ -2,11 +2,15 @@ const path = require("path")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
 
 module.exports = {
-    mode: "development",
+     mode: 'development',
     entry: "./src/index.js",
     output: {
         filename: "main.js",
         path: path.resolve(__dirname, "dist")
+    },
+    devtool: "eval-source-map",
+    devServer: {
+      watchFiles: ["./src/template.html"],
     },
     plugins: [
         new HtmlWebpackPlugin({
