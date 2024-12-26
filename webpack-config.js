d040ee97f9e17334c0c7b592dabadbf3ed24122f
zelawest <169395273+ZELAWEST@users.noreply.github.com>
2024-12-26 10:18:12 +0200

    Add webpack-config.js package.json

---
+++ webpack-config.js
@@ -0,0 +1,23 @@
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+
+module.exports = {
+    mode: "development",
+    entry: "./src/index.js",
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist")
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/template.html"
+        })
+    ],
+    module: {
+        rules: [{
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: "asset/resource",
+        }
+        ]
+    }
+}
