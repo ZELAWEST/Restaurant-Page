08366c9294c205dcfb143c24111059848b6e040e
zelawest <169395273+ZELAWEST@users.noreply.github.com>
2024-12-26 13:54:52 +0200

    Addimage to src

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
+
+import {pageLoad} from './pageload'
+pageLoad()
 console.log("Restaurant Page")
 alert("Restaurant Page")
--- webpack.config.js
+++ webpack.config.js
@@ -7,11 +7,12 @@ module.exports = {
     output: {
         filename: "main.js",
         path: path.resolve(__dirname, "dist"),
+        assetModuleFilename: "[name][ext]",
         clean: true,
     },
     devtool: "eval-source-map",
     devServer: {
-      watchFiles: ["./src/template.html"],
+        watchFiles: ["./src/template.html"],
     },
     plugins: [
         new HtmlWapPackPlugin({
@@ -25,12 +26,18 @@ module.exports = {
             test: /\.css$/i,
             use: ['style-loader', 'css-loader']
         },
+
         {
             test: /\.(png|svg|jpg|jpeg|gif)$/i,
             type: "asset/resource",
-        }
-
-
+        },
+        {
+            test: /\.(png|jpe?g|gif)$/i,
+            loader: 'file-loader',
+            options: {
+              name: '[path][name].[ext]',
+            },
+          },
 
         ]
     }
